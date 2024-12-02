df797e00fff869f14510f3fb305c46cfff396569
Vinicius Tavares <viniciustavares@declink.com.br>
2024-12-02 16:33:26 -0300

    Adicionado testes instrumentado ui

--- app/src/androidTest/java/com/example/appcicd/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/appcicd/ExampleInstrumentedTest.kt
@@ -1,5 +1,12 @@
 package com.example.appcicd
 
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,25 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.appcicd", appContext.packageName)
+    fun testEditText_ValidateTextNome(){
+        onView(withId(R.id.et_nome))
+            .perform(typeText("Teste"))
+
+
+        // Verificar se o texto foi digitado corretamente
+        onView(withId(R.id.et_nome))
+            .check(matches(withText("Teste")))
     }
+
 }
+
+// Nome: Se o nome é igual ou se o nome é diferente
+// Altura: Se altura tem mais de 3 caracteres, se é correta, se é igual a zero, se ele converte corretamente
+// Peso: se peso tem mais de 3 caracteres, oposto, se recebe letras e se é igual a zero
+// Botão: deve ser ativado somente se todos os campos estiverem preenchidos, tentativa de clicar quando um campo não está preenchido.
